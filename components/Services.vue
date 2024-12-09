5583445c2966be7f9561064542e9a7cd73902cb4
unknown <zizdjbd@gmail.com>
2024-12-09 08:32:20 +0600

    hire-me route added

---
+++ components/Services.vue
@@ -0,0 +1,38 @@
+<script setup>
+import services from "~/assets/data/services";
+
+const noticeItems = [
+  "All websites are Responsive.",
+  "SEO Friendly + Speed Optimized",
+  "Domain, Hosting / VPS Not Included.",
+  "3 Page per website, then $5-20 per page.",
+];
+</script>
+
+<template>
+  <div>
+    <section class="segment">
+      <h2>Services</h2>
+      <div class="post-preview">
+        <div
+          v-for="(service, index) in services"
+          :key="index"
+          class="anchored card"
+        >
+          <div>
+            <h1>{{ service.name }}</h1>
+            <p v-if="service.for !== 'Any'">{{ service.for }}</p>
+          </div>
+          <div class="anchored links">
+            <time>{{ service.fee }}</time>
+          </div>
+        </div>
+      </div>
+      <br />
+      <p>Please Notice</p>
+      <ul>
+        <li v-for="(item, index) in noticeItems" :key="index">{{ item }}</li>
+      </ul>
+    </section>
+  </div>
+</template>
---
+++ pages/hire-me.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="hero">
+        <h1>Hire Me</h1>
+      </div>
+      <br />
+      <p>
+        Feel free to contact me by email at
+        <a href="mailto:info+hire@zmt3.com">info+hire@zmt3.com</a>
+      </p>
+
+      <section class="segment small">
+        <div class="post-content">
+          <Services />
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
