11b01cd77ffec580dd97cf7a394c55addebff660
Zubair Ibn Zamir <zizdjbd@gmail.com>
2025-01-22 16:03:54 +0600

    error.vue

---
+++ error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <br />
+      <br />
+      <br />
+      <div style="text-align: center">
+        <h1>{{ error.statusCode }}</h1>
+        <p>Page not found</p>
+        <NuxtLink to="/">Go back home</NuxtLink>
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
